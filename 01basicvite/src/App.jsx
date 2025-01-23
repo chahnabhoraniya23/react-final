8cf9d6deff4b29d023d758cae078c477e6abc489
Chahna Bhoraniya <4chahna4@gmail.com>
2025-01-23 12:03:17 +0530

    add 01basicvite

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Youtube from "./Youtube.jsx"
+
+function App() {
+
+  return (
+    <>
+      <h1>App is here</h1>
+      <h1>only here</h1>
+      <Youtube />
+    </>
+  )
+}
+
+export default App
+
+
---
+++ 01basicvite/src/Youtube.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Youtube(){
+    return(
+        <>
+        <h1>it's me</h1>
+        <h1>again</h1>
+        </>
+    )
+}
+
+export default Youtube
